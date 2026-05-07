66c700f84d6f57feb5ac406953543895a18bff3d
UrdaniviaCamilo <nightsharklma0@gmail.com>
2026-05-07 18:22:10 -0500

    Primer commit

---
+++ src/main/java/com/semana13/hola_heroku/HolaHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.semana13.hola_heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semana13/hola_heroku/HolaHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana13.hola_heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
